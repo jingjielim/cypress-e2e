2dd8592d2f89fda1d3faab9398c48e57ccdddda7
jingjielim <jingjie3000@gmail.com>
2023-04-21 00:22:10 -0400

    Add more tests

--- 01 Starting Project/cypress/e2e/tasks.cy.js
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -3,8 +3,15 @@
 describe("tasks management", () => {
   it("should open and close the new task modal", () => {
     cy.visit("http://127.0.0.1:5174/");
+    // Cancel with clicking backdrop
     cy.contains("Add Task").click();
     cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+    
+    // Cancel with cancel button
+    cy.contains("Add Task").click();
+    cy.contains("Cancel").click();
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
