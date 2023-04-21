b0d5c49664e2ea5344733a63e2f3505c3bdda74a
jingjielim <jingjie3000@gmail.com>
2023-04-21 00:30:11 -0400

    Add a test for creating task

--- 01 Starting Project/cypress/e2e/tasks.cy.js
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -8,10 +8,23 @@ describe("tasks management", () => {
     cy.get(".backdrop").click({ force: true });
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
-    
+
     // Cancel with cancel button
     cy.contains("Add Task").click();
     cy.contains("Cancel").click();
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+  });
+
+  it("should create a new task", () => {
+    cy.visit("http://127.0.0.1:5174/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New task");
+    cy.get("#summary").type("Some description");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get(".task h2").contains("New task");
+    cy.get(".task p").contains("Some description");
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
