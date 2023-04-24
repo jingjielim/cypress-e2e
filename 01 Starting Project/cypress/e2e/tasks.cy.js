90aeb069439a8466faf0698925131e8d35233e4c
jingjielim <jingjie3000@gmail.com>
2023-04-23 23:00:18 -0400

    Selecting dropdown values

--- 01 Starting Project/cypress/e2e/tasks.cy.js
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -2,7 +2,7 @@
 
 describe("tasks management", () => {
   it("should open and close the new task modal", () => {
-    cy.visit("http://127.0.0.1:5174/");
+    cy.visit("http://127.0.0.1:5173/");
     // Cancel with clicking backdrop
     cy.contains("Add Task").click();
     cy.get(".backdrop").click({ force: true });
@@ -17,7 +17,7 @@ describe("tasks management", () => {
   });
 
   it("should create a new task", () => {
-    cy.visit("http://127.0.0.1:5174/");
+    cy.visit("http://127.0.0.1:5173/");
     cy.contains("Add Task").click();
     cy.get("#title").type("New task");
     cy.get("#summary").type("Some description");
@@ -30,9 +30,26 @@ describe("tasks management", () => {
   });
 
   it("should validate user input", () => {
-    cy.visit("http://127.0.0.1:5174/");
+    cy.visit("http://127.0.0.1:5173/");
     cy.contains("Add Task").click();
     cy.get(".modal").contains("Add Task").click();
     cy.contains("Please provide");
+  });
+
+  it("should filter tasks", () => {
+    cy.visit("http://127.0.0.1:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New task");
+    cy.get("#summary").type("Some description");
+    cy.get("#category").select("urgent");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("moderate");
+    cy.get(".task").should("have.length", 0);
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("all");
+    cy.get(".task").should("have.length", 1);
+
   });
 });
